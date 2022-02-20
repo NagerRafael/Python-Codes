2ebf635508956926e30b1c301d752825c952a47c
Rafael Naranjo <nager_rafael1998@hotmail.com>
2022-02-20 10:23:52 -0500

    more efficient version

--- Array_Diff.py
+++ Array_Diff.py
@@ -1,5 +1,2 @@
 def array_diff(a, b):
-    for x in b:         #Iterating the elements has removed
-        while x in a:   #While element x is in the list a
-            a.remove(x) #Remove element x from list a
-    return a
+    return [x for x in a if x not in b]
