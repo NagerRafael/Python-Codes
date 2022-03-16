218973fd17d4456f06462cab2c01598ae6ffb6df
Rafael_Naranjo <naranara@espol.edu.ec>
2022-03-16 00:35:51 -0500

    ParseInt(): versión de prueba completada

--- ParseInt.py
+++ ParseInt.py
@@ -2,9 +2,9 @@ def parse_int(string):
     string = string.split()[::-1]
     
     def ident(deci):
-        num = {'zero': 0, 'one': 1, 'tw': 2, 'th': 3, 'fo': 4,
-               'fi': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
-               'ten': 10, 'eleven': 11, 'twelve': 12}
+        num = {'ten': 10, 'eleven': 11, 'twelve': 12,
+               'zero': 0, 'one': 1, 'tw': 2, 'th': 3, 'fo': 4,
+               'fi': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
         num_k = num.keys()
         for n in num_k:
             if n in deci:
@@ -15,15 +15,17 @@ def parse_int(string):
     sum = 0
     carrion = 1
     for x in string:
-        if (x == 'hundred') & (sum < 1000):
-            carrion = 100
-        elif (x == 'hundred') & (sum > 1000):
+        print(x)
+        print('carrion_i: '+str(carrion))
+        if ((x == 'hundred') & ((sum > 1000) | (carrion == 1000))):
             carrion = 100000
+        elif (x == 'hundred') & (sum < 1000):
+            carrion = 100
         elif x == 'thousand':
             carrion = 1000
-            
-        print(carrion)
-        if '-' in x:
+        elif x == 'million':
+            carrion = 1000000
+        elif '-' in x:
             print('-')
             dec = x.split('-')
             sum += (ident(dec[0]) * 10 + ident(dec[1])) * carrion
@@ -40,6 +42,6 @@ def parse_int(string):
             print('solo')
             sum += ident(x) * carrion
             carrion = 1
-        
-        print(sum)
+        print('carrion_f: '+str(carrion))
+        print('sum: '+str(sum))
     return sum
